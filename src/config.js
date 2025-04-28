ce7a8a73a774d5b1c9579153bce5c6dd8494c817
Osama-tec <hashmiosama555@gmail.com>
2025-04-29 01:54:27 +0500

    #Updated: live url

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 export default {
-    // IMPORTANT: This should be a publishable key (pk_live_... or pk_test_...)
-    // Never use a secret key (sk_live_... or sk_test_...) in frontend code
-    STRIPE_PUBLIC_KEY: 'pk_live_51QhbVSBiF6yPUWJJjGbSiHhYCrwGE88aPWMLhPIq82VGnDRbo2dJ1sxcRqRplVdtE2RikEjw4vkfzJVRhLwp0atW009lGDCHo3',
-    API_URL: '',
-    BACKEND_URL: 'http://localhost:8080'
-}
-
+  // IMPORTANT: This should be a publishable key (pk_live_... or pk_test_...)
+  // Never use a secret key (sk_live_... or sk_test_...) in frontend code
+  STRIPE_PUBLIC_KEY: "pk_live_51QhbVSBiF6yPUWJJjGbSiHhYCrwGE88aPWMLhPIq82VGnDRbo2dJ1sxcRqRplVdtE2RikEjw4vkfzJVRhLwp0atW009lGDCHo3",
+  API_URL: "",
+  BACKEND_URL: "https://selliox-b.onrender.com",
+};
