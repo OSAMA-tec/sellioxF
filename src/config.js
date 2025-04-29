5ab1125fda90a8507d944fce4475c2c729569455
Osama-tec <hashmiosama555@gmail.com>
2025-04-30 01:52:33 +0500

    ff

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ export default {
   // Never use a secret key (sk_live_... or sk_test_...) in frontend code
   STRIPE_PUBLIC_KEY: "pk_live_51QhbVSBiF6yPUWJJjGbSiHhYCrwGE88aPWMLhPIq82VGnDRbo2dJ1sxcRqRplVdtE2RikEjw4vkfzJVRhLwp0atW009lGDCHo3",
   API_URL: "",
-  BACKEND_URL: "http://selliox-b.onrender.com",
+  BACKEND_URL: "https://selliox.com",
 };
